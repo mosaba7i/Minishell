91045ad4f7b8536517e86281ffbcfe9dfe9d8a57
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-02-12 13:05:06 +0300

    Add files via upload

---
+++ builtins/echo.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malsabah <malsabah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 18:40:00 by malsabah          #+#    #+#             */
+/*   Updated: 2026/02/12 12:31:27 by malsabah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+static int	is_n_flag(char *arg)
+{
+	int	i;
+
+	if (!arg || arg[0] != '-')
+		return (0);
+	i = 1;
+	while (arg[i] && arg[i] == 'n')
+		i++;
+	if (arg[i] == '\0' && i > 1)
+		return (1);
+	return (0);
+}
+
+int	ft_echo(char **args)
+{
+	int	i;
+	int	newline;
+
+	i = 1;
+	newline = 1;
+	while (args[i] && is_n_flag(args[i]))
+	{
+		newline = 0;
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
+	return (0);
+}
+
+//for testing
+int main(int ac,char **av)
+{
+    ft_echo(av);
+}
