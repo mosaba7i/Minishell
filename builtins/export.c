7d498858ede7467bbe75f993200c408807b42c60
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-02-14 04:22:20 -0800

    Add files via upload

---
+++ builtins/export.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malsabah <malsabah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 14:51:44 by malsabah          #+#    #+#             */
+/*   Updated: 2026/02/14 15:11:10 by malsabah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+ void get_key_value(char *arg, char **key, char **value)
+{
+    char *equal = ft_strchr(arg, '=');
+    if (equal)
+    {
+        *key = ft_substr(arg, 0, equal - arg);
+        *value = equal + 1;
+    }
+    else
+    {
+        *key = ft_strdup(arg);
+        *value = NULL;
+    }
+}
+static void print_export_sorted(t_env *env)
+{
+    t_env *tmp;
+    t_env *min;
+    char *last;
+    
+    last = "";
+    while (1)
+    {
+        tmp = env;
+        min = NULL;
+        while (tmp)
+        {
+            if (ft_strncmp(tmp->key, last, ft_strlen(tmp->key) + 1) > 0)
+                if (!min || ft_strncmp(tmp->key, min->key, ft_strlen(tmp->key) + 1) < 0)
+                    min = tmp;
+            tmp = tmp->next;
+        }
+        if (!min)
+            break;
+        printf("declare -x %s", min->key);
+        if (min->value)
+            printf("=\"%s\"", min->value);
+        printf("\n");
+        last = min->key;
+    }
+}
+
+
+
+void export(t_shell *shell, char **args)
+{
+    int i = 1;
+
+    if (!args[1])
+    {
+        print_export_sorted(shell->env);
+        return;
+    }
+
+    while (args[i])
+    {
+        export_var(&shell->env, args[i]);
+        i++;
+    }
+}
+
+//for testing only
+/*int main(int ac,char **av,char** envp)
+{
+    //cc export.c export_utils.c ../init_shell.c -L ../libft -lft
+    t_shell *shelly;
+    shelly = init_shell(envp);
+    export(shelly,av);
+}*/
---
+++ builtins/export_utils.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malsabah <malsabah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 15:11:00 by malsabah          #+#    #+#             */
+/*   Updated: 2026/02/14 15:09:08 by malsabah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+static int is_valid_identifier(char *str)
+{
+    int i = 0;
+
+    if (!str || (!ft_isalpha(str[0]) && str[0] != '_'))
+        return (0);
+    while (str[i] && str[i] != '=')
+    {
+        if (!ft_isalnum(str[i]) && str[i] != '_')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+static t_env *find_env(t_env *env, char *key)
+{
+    while (env)
+    {
+        if (ft_strncmp(env->key, key,ft_strlen(key)) == 0)
+            return env;
+        env = env->next;
+    }
+    return NULL;
+}
+
+static t_env *create_env_node(char *key, char *value)
+{
+    t_env *new = malloc(sizeof(t_env));
+    if (!new)
+        return NULL;
+    new->key = ft_strdup(key);
+    if (value)
+        new->value = ft_strdup(value);
+    else
+        new->value = NULL;
+    new->next = NULL;
+    return new;
+}
+
+static void add_env_back(t_env **env, t_env *new)
+{
+    t_env *tmp;
+    if (!*env)
+    {
+        *env = new;
+        return;
+    }
+    tmp = *env;
+    while (tmp->next)
+        tmp = tmp->next;
+    tmp->next = new;
+}
+void export_var(t_env **env, char *arg)
+{
+    char *equal;
+    char *key;
+    char *value;
+    t_env *existing;
+
+    if (!is_valid_identifier(arg))
+    {
+        printf("minishell: export: `%s': not a valid identifier\n", arg);
+        return;
+    }
+    equal = ft_strchr(arg, '=');
+    get_key_value(arg, &key, &value);
+    existing = find_env(*env, key);
+    if (existing && equal)
+    {
+            free(existing->value);
+            existing->value = ft_strdup(value);
+    }
+    else
+        add_env_back(env, create_env_node(key, value));
+    free(key);
+}
