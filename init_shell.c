01122da2144bfc335978d3841704ede9dcfe0eef
Mohammed Al-Sabahi <mosaba7i03@gmail.com>
2026-02-14 04:23:01 -0800

    Add files via upload

---
+++ init_shell.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_shell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malsabah <malsabah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 17:40:14 by malsabah          #+#    #+#             */
+/*   Updated: 2026/02/13 17:40:55 by malsabah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_shell *init_shell(char **envp)
+{
+    t_shell *shell = malloc(sizeof(t_shell));
+    if (!shell)
+        return NULL;
+
+    shell->env = NULL;
+    shell->exit_status = 0;
+
+    int i = 0;
+    while (envp[i])
+    {
+        export_var(&shell->env, envp[i]);
+        i++;
+    }
+    return shell;
+}
+
+void free_env(t_shell *shell)
+{
+    t_env *tmp;
+    while (shell->env)
+    {
+        tmp = shell->env;
+        shell->env = shell->env->next;
+        free(tmp->key);
+        free(tmp->value);
+        free(tmp);
+    }
+    free(shell);
+}
