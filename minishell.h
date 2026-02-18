9f7b34cdfaa1c7fb9c36e678f0979841236b21e7
Lialliane <lojain01k@gmail.com>
2026-02-18 15:05:46 +0300

    finished tokenization

--- minishell.h
+++ minishell.h
@@ -11,44 +11,46 @@
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# define MINISHELL_H
+#define MINISHELL_H
 
-#include <readline/readline.h>      // readlinne (ext fun)
-#include <readline/history.h>       // add_history, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay (ext func)
-#include <stdio.h>                  // printf, perror, strerror
-#include <stdlib.h>                 // malloc, free, exit, getenv
-#include <unistd.h>                 // write, read, fork, execve, pipe, dup, dup2, chdir, getcwd, close
-#include <fcntl.h>                  // open, O_RDONLY, O_WRONLY, O_CREAT, O_APPEND, O_TRUNC
-#include <sys/wait.h>               // wait, waitpid, wait3, wait4
-#include <signal.h>                 // signal, sigaction, kill, sigemptyset, sigaddset
-#include <dirent.h>                 // opendir, readdir, closedir
+#include "libft/libft.h"
+#include <readline/readline.h> // readlinne (ext fun)
+#include <readline/history.h>  // add_history, rl_clear_history, rl_on_new_line, rl_replace_line, rl_redisplay (ext func)
+#include <stdio.h>			   // printf, perror, strerror
+#include <stdlib.h>			   // malloc, free, exit, getenv
+#include <unistd.h>			   // write, read, fork, execve, pipe, dup, dup2, chdir, getcwd, close
+#include <fcntl.h>			   // open, O_RDONLY, O_WRONLY, O_CREAT, O_APPEND, O_TRUNC
+#include <sys/wait.h>		   // wait, waitpid, wait3, wait4
+#include <signal.h>			   // signal, sigaction, kill, sigemptyset, sigaddset
+#include <dirent.h>			   // opendir, readdir, closedir
 
-/*ext func like readline can be installed with 
+/*ext func like readline can be installed with
 sudo apt-get update
 sudo apt-get install libreadline-dev
 */
+
 void echo(char *s);
 void pwd(void);
 
 typedef struct s_redir
 {
-	char				*file;
-	int					type;
-	int					fd;
-	struct s_redir		*next;
-}	t_redir;
+	char *file;
+	int type;
+	int fd;
+	struct s_redir *next;
+} t_redir;
 
-typedef	struct s_cmd 
+typedef struct s_cmd
 {
-     int infile; //fd for infile
-     int outfile;
-     char **args;
-    t_redir	*redirs;
-    int pipe[2];
-     struct s_cmd *next;
- }   t_cmd;
+	int infile; // fd for infile
+	int outfile;
+	char **args;
+	t_redir *redirs;
+	int pipe[2];
+	struct s_cmd *next;
+} t_cmd;
 
- typedef enum e_type
+typedef enum e_type
 {
 	WORD,
 	PIPE,
@@ -61,16 +63,15 @@ typedef	struct s_cmd
 typedef struct s_token
 {
 	t_type type;
-	char   *value;
+	char *value;
 	struct s_token *next;
 } t_token;
 
-
 typedef struct s_commnad
 {
-	char   **arg_lst;
-	char   *in_fd;
-	char   *out_fd;
+	char **arg_lst;
+	char *in_fd;
+	char *out_fd;
 	struct s_commnad *next;
 } t_commnad;
 
--- token.c
+++ token.c
@@ -1,88 +1,182 @@
-#include <readline/history.h>
-#include <readline/readline.h>
-#include <stdio.h>
-#include <unistd.h>
 
 #include "minishell.h"
 
-
-char *qoute_string(char *start);
+int is_white_space(char c);
+t_token *new_token(void *content);
+int extract_word(char *input, int i);
+t_token *tokenize(char *input);
+void append_token(t_token **head, t_token *new);
+void print_tokens(t_token *head);
+void assign_token_types(t_token *head);
+int is_operator(char c);
+int get_operator(char *input, int word);
 
 int main(int argc, char **argv, char **envp)
 {
-
 	while (1)
 	{
 		char *inpt_line = readline("minishell$ ");
 		if (!inpt_line) // ctrl+D
 			return (1);
 		if (*inpt_line)
-		{	
-			//add_history(inpt_line);
-			printf("%s\n", inpt_line);
-		}
-		//t_token *tokens = tokenize(line);
-		//s_commnad *cmds   = parse(tokens);
+			add_history(inpt_line);
+		t_token *tokens = tokenize(inpt_line);
+		print_tokens(tokens); // TODO: remove when done debugging
 
-		//execute(cmds);
+		// s_commnad *cmds   = parse(tokens);
 
-		//free_all(tokens, cmds, line);
+		// execute(cmds);
+
+		// free_all(tokens, cmds, line);
 	}
 }
 
-t_token tokenize(char *input)
+t_token *tokenize(char *input)
 {
 	int i = 0;
-	char *words;
-	t_token head;
-	t_token current;
+	t_token *head;
+	t_token *current_token;
+	int end_pos;
 
-	words = ft_split(input, ' ');
-	current = head;
-	while(words[i])
+	head = NULL;
+	while (input[i])
 	{
-		current.value = NULL;
-		if(ft_strncmp(words[i], "|", 2))
-			current.type = PIPE;
-		else if(ft_strncmp(*words, "<", 2))
-			current.type = REDIR_IN;
-		else if(ft_strncmp(words[i], ">", 2))
-			current.type = REDIR_OUT;
-		else if(ft_strncmp(words[i], "<<", 3))
-			current.type = HEREDOC;
-		if(ft_strncmp(words[i], ">>", 3))
-			current.type = REDIR_OUT_APP;
-		if(ft_strncmp(words[i], '"', 1))
-		{
-			current.type = WORD;
-			// TODO: multiple qoutation? expansion logic in single/double, skip words until
-			// next quotation
-			current.value = qoute_string(ft_strrchr(input, '"'));
-		}
-		else
-		{	
-			current.type = WORD;
-			current.value = ft_strdup(words[i]);
-		}
-		current = current.next;
-		i++;
+		while (is_white_space(input[i]))
+			i++;
+		if (!input[i])
+			break;
+		end_pos = extract_word(input, i);
+		current_token = new_token(ft_substr(input, i, end_pos - i));
+		append_token(&head, current_token);
+		i = end_pos;
 	}
-	// TODO: freeall
+	assign_token_types(head);
 	return (head);
 }
 
-char *qoute_string(char *start)
+int is_white_space(char c)
 {
-	char *word;
-	int i;
-
-	i = 0;
-	while(start[i] && start[i] != "")
-		i++;
-	if(start[i] == "")
-		return ft_substr(start, 0, i);
-	else
-		return (NULL);
+	return c == ' ' || (c >= 9 && c <= 13);
 }
 
+int is_operator(char c)
+{
+	return (c == '<' || c == '>' || c == '|');
+}
 
+int extract_word(char *input, int i)
+{
+	char inside_qoute = 0;
+	int word;
+
+	word = i;
+	if (is_operator(input[word]))
+		return get_operator(&input[word], word);
+	inside_qoute = 0;
+	while (input[word])
+	{
+		// printf("word: %d character: %c \n", word, input[word]);
+		// printf("inside_qoute?  %d  (%c)  white_space? %d \n", inside_qoute, inside_qoute, is_white_space(input[word]));
+		if (!inside_qoute && is_operator(input[word]))
+			break;
+		else if (!inside_qoute && (input[word] == '\'' || input[word] == '\"'))
+			inside_qoute = input[word];
+		else if (!inside_qoute && is_white_space(input[word]))
+			break;
+		else if (inside_qoute && input[word] == inside_qoute)
+			inside_qoute = 0;
+		word++;
+	}
+	if (inside_qoute)
+	{
+		printf("\nno end of qoute >:( \n"); // TODO: combine errors into one function
+		exit(1);
+	}
+	return (word);
+}
+
+int get_operator(char *input, int word)
+{
+	// printf("input: %s \n", input);
+	if (!ft_strncmp(input, "<<", 2) || !ft_strncmp(input, ">>", 2))
+		word += 2;
+	else
+		word++;
+	return word;
+}
+
+t_token *new_token(void *content)
+{
+	t_token *list_node;
+
+	list_node = malloc(sizeof(t_token));
+	if (!list_node)
+		return (NULL);
+	list_node->next = NULL;
+	list_node->value = content;
+	return (list_node);
+}
+
+void append_token(t_token **head, t_token *new)
+{
+	t_token *last;
+
+	if (!(*head))
+	{
+		*head = new;
+		return;
+	}
+	last = *head;
+	while (last->next)
+		last = last->next;
+	last->next = new;
+}
+t_type get_token_type(char *value)
+{
+	if (!value || !value[0])
+		return (WORD);
+
+	if (!ft_strncmp(value, "<<", 3))
+		return (HEREDOC);
+	if (!ft_strncmp(value, ">>", 3))
+		return (REDIR_OUT_APP);
+	if (!ft_strncmp(value, "<", 2))
+		return (REDIR_IN);
+	if (!ft_strncmp(value, ">", 2))
+		return (REDIR_OUT);
+	if (!ft_strncmp(value, "|", 2))
+		return (PIPE);
+	return (WORD);
+}
+
+void assign_token_types(t_token *head)
+{
+	t_token *current = head;
+
+	while (current)
+	{
+		current->type = get_token_type(current->value);
+		current = current->next;
+	}
+}
+
+void print_tokens(t_token *head)
+{
+	while (head)
+	{
+		printf("value: [%s]  type: %d ", head->value, head->type);
+		if (head->type == 0)
+			printf(" %s\n", "WORD");
+		if (head->type == 1)
+			printf(" %s\n", "PIPE");
+		if (head->type == 2)
+			printf(" %s\n", "REDIR_IN");
+		if (head->type == 3)
+			printf(" %s\n", "REDIR_OUT");
+		if (head->type == 4)
+			printf(" %s\n", "REDIR_OUT_APP");
+		if (head->type == 5)
+			printf(" %s\n", "HEREDOC");
+		head = head->next;
+	}
+}
