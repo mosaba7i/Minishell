8acc94f38e73ca432beae876242aa4eb36b4ea54
Lialliane <lojain01k@gmail.com>
2026-04-06 13:55:12 +0300

    added subtitution

--- parsing/remove_quotes.c
+++ parsing/remove_quotes.c
@@ -1,4 +1,3 @@
-
 #include "../minishell.h"
 
 int count_quotes(char *arg);
@@ -6,6 +5,7 @@ void get_quotes(char **arg, char **quote_pos);
 int is_quote(char *pos, char **quote_pos);
 char *remove_quotes(char **arg, char **quote_pos, t_shell *shell);
 void search_remove_quotes(char **arg, t_shell *shell);
+void resubstitute_expand_char(char **arg);
 
 void handle_quotes(t_command *cmds, t_shell *shell)
 {
@@ -18,16 +18,37 @@ void handle_quotes(t_command *cmds, t_shell *shell)
 		while (cmds->arg_lst[i])
 		{
 			search_remove_quotes(&cmds->arg_lst[i], shell);
+			resubstitute_expand_char(&cmds->arg_lst[i]);
 			i++;
 		}
 		redir_temp = cmds->redirs;
 		while (redir_temp)
 		{
 			if (redir_temp->file)
+			{
 				search_remove_quotes(&redir_temp->file, shell);
+				resubstitute_expand_char(&redir_temp->file);
+			}
 			redir_temp = redir_temp->next;
 		}
 		cmds = cmds->next;
+	}
+}
+
+void resubstitute_expand_char(char **arg)
+{
+	int i;
+
+	i = 0;
+	while ((*arg)[i])
+	{
+		if ((*arg)[i] == X_SQUOTE)
+			(*arg)[i] = '\'';
+		else if ((*arg)[i] == X_DQUOTE)
+			(*arg)[i] = '\"';
+		else if ((*arg)[i] == X_DOLLAR)
+			(*arg)[i] = '$';
+		i++;
 	}
 }
 
